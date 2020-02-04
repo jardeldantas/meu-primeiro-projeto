8b032a449488e5408af3293aaa75b24cd21e3f1f
jardeldantas <jarde.dantas@outlook.com>
2020-02-04 07:53:05 -0300

    Estudando Spring - versão Inicial do Projeto.

---
+++ src/test/java/com/kazale/api/MeuPrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kazale.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
